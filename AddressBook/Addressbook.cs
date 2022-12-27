3eb1204e8714a27c62074b1fc4e459a96837856c
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-27 08:50:32 +0530

    [ajay]-Added bacis contect

---
+++ AddressBook/Addressbook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Addressbook
+    {
+        public string FirstName, LastName, Address, City, State , Email;
+        public int Zip;
+        public long PhoneNumber;
+        public Addressbook(string firstName, string lastName, string address, string city, string state,string Email, int zip, long phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Email = Email;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+        }
+        public void DisplayContect()
+        {
+            Console.WriteLine("Name {0} {1},",FirstName,LastName);
+            Console.WriteLine("Adrdess {0} " , Address);
+            Console.WriteLine("City {0}", City);
+            Console.WriteLine("State {0} ", State);
+            Console.WriteLine("Email {0}", Email);
+            Console.WriteLine("Zip {0}", Zip);
+            Console.WriteLine("PhoneNumber {0}", PhoneNumber);
+        }
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Address Book program");
+            Addressbook Add = new Addressbook("Ajay","Singh","Sector 45","Gurgaon","Haryana","ajay8462@gamil.com",402001,7898234604);
+            Add.DisplayContect();
+
+        }
+    }
+}
